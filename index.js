144a57124aebcd9bf41de891a857237474c99784
vanessaformiga <vanessandrade16@gmail.com>
2024-05-28 13:48:01 -0300

    enviando os arquivos do projeto

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
+// Configurando o diretório de arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-    res.send("Realizando a conexão");
+    res.render("index");
 });
+
+
+
 
 app.listen(8082, () => {
     console.log("Iniciando projeto");
